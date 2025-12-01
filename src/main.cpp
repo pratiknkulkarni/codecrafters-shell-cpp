d1ad92036de8f0f5da2cdf8cebdeecdc478ea8bd
Pratik Kulkarni <pnkulkarni@proton.me>
2025-12-01 18:01:34 +0530

    testing for stage IZ3

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,60 @@
 #include <iostream>
+#include <ranges>
 #include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
+
+
+std::vector<std::string> tokenize(const string &line) {
+    vector<string> tokens;
+    stringstream ss(line);
+    string token;
+
+    while (ss >> token) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
 
 void exitCommand() {
     exit(0);
 }
 
+void echoCommand(vector<string> args) {
+    string echoOutput;
+    for (auto arg: args) {
+        echoOutput += arg;
+        echoOutput += " ";
+    }
+    cout << echoOutput << endl;
+}
+
 void repl() {
-    string command;
+    string userInput;
+
     while (true) {
         cout << "$ ";
-        cin >> command;
+        if (!getline(cin, userInput)) {
+            break;
+        }
+        auto tokens = tokenize(userInput);
+        if (tokens.empty()) {
+            continue;
+        }
+
+        const string &command = tokens[0];
+        vector<string> arguments(tokens.begin() + 1, tokens.end());
 
         if (command == "exit") {
             exitCommand();
+        } else if (command == "echo") {
+            echoCommand(arguments);
+        } else {
+            cout << userInput << ": command not found" << endl;
         }
-
-        cout << command << ": command not found" << endl;
     }
 }
 
